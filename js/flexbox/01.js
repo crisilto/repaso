2d4f1f04d592d177531991ba60e85dd9346fb056
crisilto <crisilto1997@gmail.com>
2024-03-10 01:52:04 +0100

    flex & grid

--- js/flexbox/01.js
+++
@@ -1,2 +0,0 @@
-//Practica implementar un layout de página usando Flexbox con JavaScript dinámicamente para entender mejor cómo se pueden manipular 
-//los estilos con JS.
--- js/grid/01.js
+++
@@ -1,2 +0,0 @@
-//Practica implementar un layout de página usando Grid con JavaScript dinámicamente para entender mejor cómo se pueden manipular 
-//los estilos con JS.
