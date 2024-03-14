93beb02d856d105316dbb1ca527c2bddcca5e4d5
crisilto <crisilto1997@gmail.com>
2024-03-14 08:24:00 +0100

    sql basics

--- sql/conceptos_basicos/basicos.sql
+++ sql/conceptos_basicos/basicos.sql
@@ -1,12 +1,26 @@
 --• Crear una tabla: Escribe una consulta SQL para crear una tabla simple con varios campos, incluyendo diferentes tipos de datos como texto, 
 --  número entero, fecha, etc.
-
+CREATE TABLE Usuarios (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(50),
+  apellido VARCHAR(50),
+  email VARCHAR(100) UNIQUE,
+  fecha_nacimiento DATE,
+  saldo DECIMAL(10, 2) -- 10 dígitos en total, 2 de ellos son decimales
+);
 
 --• Insertar datos: Inserta algunos datos de muestra en la tabla que creaste utilizando la instrucción INSERT INTO.
+INSERT INTO Usuarios (nombre, apellido, email, fecha_nacimiento, saldo) VALUES 
+('Laura', 'García', 'laura.garcia@example.com', '1995-08-15', 100.50),
+('Carlos', 'Martínez', 'carlos.martinez@example.com', '1992-04-22', 150.75),
+('Ana', 'López', 'ana.lopez@example.com', '1998-11-30', 200.00);
 
 
 --• Actualizar datos: Actualiza algunos registros en la tabla utilizando la instrucción UPDATE.
-
+UPDATE Usuarios
+SET saldo = 250.75, email = 'nuevo.email@example.com'
+WHERE nombre = 'Laura' AND apellido = 'García';
 
 --• Eliminar datos: Elimina registros específicos de la tabla utilizando la instrucción DELETE FROM.
-
+DELETE FROM Usuarios
+WHERE nombre = 'Carlos' AND apellido = 'Martínez';
--- sql/consultas_SELECT/consultas.sql
+++ sql/consultas_SELECT/consultas.sql
@@ -1,14 +1,143 @@
---• Consulta básica: Escribe una consulta SELECT simple para recuperar todos los registros de una tabla específica.
+--• Consulta básica
+--Devuelve todos los registros de una tabla específica.
+SELECT
+    *
+FROM
+    Usuarios;
+
+--Devuelve el nombre y apellido de la tabla Usuarios
+SELECT
+    nombre,
+    apellido
+FROM
+    Usuarios;
 
 
---• Filtrado de resultados: Utiliza la cláusula WHERE para filtrar los resultados de la consulta según ciertos criterios, 
---  como una condición de igualdad, desigualdad o comparación.
+--• Filtrado de resultados: Utiliza la cláusula WHERE para filtrar los resultados de la consulta según ciertos criterios, como una condición de igualdad, desigualdad o comparación.
+--Por igualdad
+SELECT
+    *
+FROM
+    Usuarios
+WHERE
+    saldo = 200.00; --Selecciona todos los datos de los usuarios cuyo saldo es específicamente 200.00
+
+--Por desigualdad
+SELECT
+    *
+FROM
+    Usuarios
+WHERE
+    saldo <> 200.00; --Selecciona todos los datos de los usuarios cuyo saldo no sea 200.00
+
+--Condiciones combinadas (BETWEEN AND/ AND)
+SELECT
+    *
+FROM
+    Usuarios
+WHERE
+    fecha_nacimiento BETWEEN '1990-01-01'
+    AND '1999-12-31'
+    AND saldo > 100.00; --Selecciona todos los datos de los usuarios nacidos entre el 90 y 99 y que tienen mas de 100.00 de saldo
+
+--Uso de OR
+SELECT
+    *
+FROM
+    Usuarios
+WHERE
+    saldo = 200.00
+    OR saldo = 150.75; --Selecciona todos los datos de los usuarios cuyo saldo es 200.00 o 150.75
+
+--Excluir un rango con NOT
+SELECT
+    *
+FROM
+    Usuarios
+WHERE
+    NOT (
+        saldo BETWEEN 100.00
+        AND 200.00
+    ); --Selecciona todos los datos de los usuarios cuayo saldo no está entre 100.00 y 200.00
 
 
---• Ordenación de resultados: Utiliza la cláusula ORDER BY para ordenar los resultados de la consulta según uno o más campos, 
---  ya sea de forma ascendente o descendente.
+--• Ordenación de resultados: Utiliza la cláusula ORDER BY para ordenar los resultados de la consulta según uno o más campos, ya sea de forma ascendente o descendente.
+--Ordenar por un campo
+SELECT
+    *
+FROM
+    Usuarios
+ORDER BY
+    apellido ASC; --Selecciona todos los datos de los usuarios ordenados por su apellido de la A a la Z
+
+--Ordenar por un campo en orden descendente
+SELECT
+    *
+FROM
+    Usuarios
+ORDER BY
+    apellido DESC; --Selecciona todos los datos de los usuarios ordenados por su apellido de la Z a la A
+
+--Ordenar por múltiples campos
+SELECT
+    *
+FROM
+    Usuarios
+ORDER BY
+    apellido ASC,
+    nombre ASC; --Si hay múltiples usuarios con el mismo apellido, sus nombres se ordenarán de la A a la Z
+
+--Combinar orden ascendente y descendente
+SELECT
+    *
+FROM
+    Usuarios
+ORDER BY
+    saldo DESC,
+    nombre ASC; --Ordena por el saldo de mayor a menor, si los hay con mismo saldo, se ordena por el nombre de la A a la Z
 
 
---• Agrupación de resultados: Utiliza la cláusula GROUP BY junto con funciones de agregación como COUNT(), SUM(), AVG(), etc., 
---  para realizar cálculos sobre grupos de datos.
+--• Agrupación de resultados: Utiliza la cláusula GROUP BY junto con funciones de agregación como COUNT(), SUM(), AVG(), etc., para realizar cálculos sobre grupos de datos.
+--Contar registros por grupo
+SELECT
+    fecha_nacimiento,
+    COUNT(*) AS numero_de_usuarios
+FROM
+    Usuarios
+GROUP BY
+    fecha_nacimiento; --Agrupa a los usuarios por su fecha de nacimiento y contará cuántos usuarios hay en cada grupo de fecha de nacimiento
 
+--Sumar valores de un grupo
+SELECT
+    apellido,
+    SUM(saldo) AS saldo_total
+FROM
+    Usuarios
+GROUP BY
+    apellido; --Muestra el saldo total para cada apellido presente en la tabla Usuarios
+
+--Calcular el promedio de un grupo
+SELECT
+    AVG(saldo) AS saldo_promedio
+FROM
+    Usuarios; --Saldo promedio entre todos los usuarios.
+
+--Saldo promedio pero agrupado por años de nacimiento
+SELECT
+    YEAR(fecha_nacimiento) AS año_nacimiento,
+    AVG(saldo) AS saldo_promedio
+FROM
+    Usuarios
+GROUP BY
+    YEAR(fecha_nacimiento); --Agrupa los usuarios por el año de nacimiento y calcula el saldo promedio en cada grupo de año.
+
+--Combinar GROUP BY con ORDER BY
+SELECT
+    apellido,
+    COUNT(*) AS numero_de_usuarios
+FROM
+    Usuarios
+GROUP BY
+    apellido
+ORDER BY
+    numero_de_usuarios DESC; --Cuenta el número de usuarios por apellido y luego ordena los resultados en orden descendente según el número de usuarios, mostrando los apellidos más comunes primero.
